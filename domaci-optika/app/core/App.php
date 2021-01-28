7312d0e1333f899b0ccc614a4ff773046a84709e
marianscupak <scupak.m@gmail.com>
2021-01-28 21:51:52 +0100

    fix

--- domaci-optika/app/core/App.php
+++ domaci-optika/app/core/App.php
@@ -2,7 +2,7 @@
 
 class App {
     // Class that handles the routing.
-    protected $controller = 'domu';
+    protected $controller = 'Domu';
     protected $method = 'index';
     protected $params = [];
 
@@ -10,8 +10,8 @@ class App {
         $url = $this->parseUrl();
 
         if (isset($url[0])) {
-            if (file_exists('../app/controllers/' . $url[0] . '.php')) {
-                $this->controller = $url[0];
+            if (file_exists('../app/controllers/' . ucfirst($url[0]) . '.php')) {
+                $this->controller = ucfirst($url[0]);
                 unset($url[0]);
             }
         }
