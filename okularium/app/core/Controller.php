fcac85a1bc395a0964816df82a6ffbe31f2aa591
marianscupak <67097396+marianscupak@users.noreply.github.com>
2021-05-03 14:42:00 +0200

    Hotfix

--- okularium/app/core/Controller.php
+++ okularium/app/core/Controller.php
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-class Controller {
-    // Base class for controllers.
-    protected function model($model) {
-        require_once '../app/models/' . $model . '.php';
-        return new $model();
-    }
-
-    public function view($view, $data = '') {
-        require_once '../app/views/' . $view . '.php';
-    }
-
-    public function viewToVar($view, $data = '') {
-        ob_start();
-        require_once '../app/views/' . $view . '.php';
-        return ob_get_clean();
-    }
-=======
 <?php
 
 class Controller {
@@ -36,5 +16,4 @@ class Controller {
         require_once './app/views/' . $view . '.php';
         return ob_get_clean();
     }
->>>>>>> routes-update
 }
