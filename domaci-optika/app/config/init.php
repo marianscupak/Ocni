3a3ca6c7f3cc3c58dc730b1552793aec05d44290
Marian Ščupák <scupak.m@gmail.com>
2021-05-03 03:05:12 +0200

    Routing reworked

--- domaci-optika/app/config/init.php
+++ domaci-optika/app/config/init.php
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-require_once '../../vendor/autoload.php';
-require_once '../app/config/database.php';
-
-require_once '../app/core/App.php';
-require_once '../app/core/Controller.php';
-=======
 <?php
 
 require_once './../vendor/autoload.php';
@@ -19,4 +10,3 @@ define('OK_LINK_PREFIX', "http://localhost/Ocni/okularium");
 define('SHARED_RES_PREFIX', "/Ocni/shared_resources");
 define('DO_RES_PREFIX', "/Ocni/domaci-optika/public");
 define('OK_RES_PREFIX', "/Ocni/okularium/public");
->>>>>>> routes-update
