0b1026342d56511562692227eb8bc8d168bac6e8
Marian Ščupák <scupak.m@gmail.com>
2021-05-03 16:35:24 +0200

    Hotfix

--- okularium/app/config/database.php
+++ okularium/app/config/database.php
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-<?php
-// Database config.
-
-use Illuminate\Database\Capsule\Manager as Capsule;
-
-$capsule = new Capsule();
-
-$capsule->addConnection([
-    'driver' => 'mysql',
-    'host' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'database' => 'ocni',
-    'charset' => 'utf8'
-]);
-
-$capsule->bootEloquent();
-=======
 <?php
 // Database config.
 
@@ -34,4 +15,3 @@ $capsule->addConnection([
 ]);
 
 $capsule->bootEloquent();
->>>>>>> routes-update
